3291f18c165c8a47c3b9f95b485a5db0b2552921
200445028 <200445028@stud.buct.edu.cn>
2019-06-07 09:16:30 +0800

    d

--- dataset/default.py
+++ dataset/default.py
@@ -31,24 +31,27 @@ def get_default_info():
 class DefaultDataset(Dataset):
     def __init__(self,
                  ):
-        struc_csv_dir = 'data/scalar_c/data/structures.csv'
-        train_csv_dir = 'data/scalar_c/data/train.csv'
+        struc_csv_dir = 'data/structures.csv'
+        train_csv_dir = 'data/train.csv'
         df_struc = pd.read_csv(struc_csv_dir)
     
         self.model_info_list = []
         pre_mol_name = ''
-        model_info = []
+        model_info = get_default_info()
         for i, row in tqdm.tqdm(df_struc.iterrows()):
             molecule_name = row['molecule_name']
-            if molecule_name != pre_mol_name:
+            if pre_mol_name != molecule_name:
                 if len(model_info) > 0:
                     self.model_info_list.append(model_info)
+                    print('model_info ', model_info)
                 #开始一个新modedel
                 model_info = get_default_info()
+                pre_mol_name = molecule_name
             
             atom_index = int( row['atom_index'])
             atom = row['atom']
             x = row['x']
             y = row['y']
             z = row['z']
+            print('ai ', atom_index, 'a ', atom, ' x ', x, ' y ', y, ' z ', z)
             model_info[atom_index] = (atom_index_dic[atom], x, y, z)
