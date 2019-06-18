12372b2dd7af44abdb3d0af1e7b607068acdb022
rosaann <147429240@qq.com>
2019-06-18 16:55:07 +0800

    d

--- dataset/default.py
+++ dataset/default.py
@@ -55,7 +55,7 @@ class DefaultDataset(Dataset):
                 self.gt_list.append(self.gt_data_list[self.random_index_list[i]])
             self.data_list = np.array(self.data_list)
             print('self.data_list ', self.data_list.shape)
-            self.data_list = self.data_list.transpose(0, 2, 1)
+            self.data_list = self.data_list.transpose(0, 3, 1, 2)
             print('self.data_list2 ', self.data_list.shape)
             self.gt_list = np.array(self.gt_list)
             print('self.gt_list ', self.gt_list.shape)
