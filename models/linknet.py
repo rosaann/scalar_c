0d0e1b0f156629d8b9e84de17751e57ad0b3fb5a
rosaann <147429240@qq.com>
2019-06-20 16:36:12 +0800

    d

--- models/linknet.py
+++ models/linknet.py
@@ -118,7 +118,7 @@ class LinkNet(nn.Module):
         self.conv2 = nn.Sequential(nn.Conv2d(32, 32, 3, 1, 1),
                                 nn.BatchNorm2d(32),
                                 nn.ReLU(inplace=True),)
-        self.tp_conv2 = nn.ConvTranspose2d(32, n_classes, 2, 2, 0)
+        self.tp_conv2 = nn.ConvTranspose2d(29, n_classes, 2, 2, 0)
         self.lsm = nn.LogSoftmax(dim=1)
 
 
