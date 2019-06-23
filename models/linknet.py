b1107f34e15efec3f4d172221404ce6f5eeefac8
200445028 <200445028@stud.buct.edu.cn>
2019-06-23 21:12:02 +0800

    d

--- models/linknet.py
+++ models/linknet.py
@@ -118,7 +118,7 @@ class LinkNet(nn.Module):
         self.conv2 = nn.Sequential(nn.Conv2d(29, 29, 3, 1, 1),
                                 nn.BatchNorm2d(29),
                                 nn.ReLU(inplace=True),)
-        self.tp_conv2 = nn.ConvTranspose2d(29, n_classes, 2, 2, 0)
+        self.tp_conv2 = nn.ConvTranspose2d(29, n_classes, 2, 2, 1)
         self.lsm = nn.LogSoftmax(dim=1)
 
 
