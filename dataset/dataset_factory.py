66030d082a34fd7ac1ef44475b884b3a383badde
rosaann <147429240@qq.com>
2019-06-24 15:36:52 +0800

    d

--- dataset/dataset_factory.py
+++ dataset/dataset_factory.py
@@ -30,6 +30,6 @@ def get_dataloader( batch_size, split, **_):
                             shuffle=is_train,
                             batch_size=batch_size,
                             drop_last=is_train,
-                            num_workers=0,
+                            num_workers=6,
                             pin_memory=False)
     return dataloader
