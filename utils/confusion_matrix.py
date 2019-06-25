1e244253c5c6f08995abba5736b749cb7fa724a9
rosaann <147429240@qq.com>
2019-06-25 16:56:58 +0800

    d

--- utils/confusion_matrix.py
+++ utils/confusion_matrix.py
@@ -19,7 +19,7 @@ class ConfusionMatrix:
 
     def update_matrix(self, target, prediction):
         print('target ', target.shape)
-        print('prediction ', prediction)
+        print('prediction ', prediction.shape, ' ', prediction)
         if not(isinstance(prediction, np.ndarray)) or not(isinstance(target, np.ndarray)):
             print("Expecting ndarray")
         elif len(target.shape) == 3:          # batched spatial target
