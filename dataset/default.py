91a8b853a5a2f147138d33daaf4398859d132d99
200445028 <200445028@stud.buct.edu.cn>
2019-06-27 22:32:34 +0800

    d

--- dataset/default.py
+++ dataset/default.py
@@ -275,12 +275,12 @@ class DefaultDataset(Dataset):
         if split == 'val':
             self.val_data_list = []
             for i in random_index_list[n7 : ]:
-                self.val_data_list.append(changeListFormToRectForm(train_data_list[random_index_list[i]]))
+                self.val_data_list.append(train_data_list[random_index_list[i]])
                 self.gt_list.append(gt_data_list[random_index_list[i]])
             self.val_data_list = np.array(self.val_data_list)
             tshape = self.val_data_list.shape
             print('self.data_list ', tshape)
-            self.val_data_list = self.val_data_list.reshape(tshape[0],tshape[3], tshape[1], tshape[2])
+            self.val_data_list = self.val_data_list.reshape(tshape[0],tshape[2], tshape[1])
             print('self.data_list2 ', self.val_data_list.shape)
             self.val_data_list = np.array(self.val_data_list)
             self.gt_list = np.array(self.gt_list)
