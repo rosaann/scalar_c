11d28332778c598433c3ee61f30267a5a9b124aa
200445028 <200445028@stud.buct.edu.cn>
2019-06-30 07:57:54 +0800

    d

--- test.py
+++ test.py
@@ -94,7 +94,7 @@ def main():
     
     optimizer_segmenter = get_optimizer(config.optimizer_segmenter.name, model_segmenter.parameters(), config.optimizer_segmenter.params)
     ####
-    checkpoint = get_model_saved(config.train_segmenter.dir, 1318)
+    checkpoint = get_model_saved(config.train_segmenter.dir, 1317)
     best_epoch, step = load_checkpoint(model_segmenter, optimizer_segmenter, checkpoint)
     
 
