5fc3e3e9806043ee5ec7ba1e9971fb722824efb2
rosaann <147429240@qq.com>
2019-07-11 11:50:26 +0800

    d

--- models/x_resnet.py
+++ models/x_resnet.py
@@ -465,7 +465,7 @@ class XResNet(nn.Module):
        # self.lineLayer30q = nn.Linear(66176, 68992)
         
        ###
-        self.lineLayer_end = nn.Linear(48992, 841)
+        self.lineLayer_end = nn.Linear(38880, 841)
         
         
       #  self.layers_y1 = nn.Sequential(nn.Conv2d(1, 4, (2,1), 1, 1),
