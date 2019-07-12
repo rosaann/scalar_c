0b5c1bd8a3e269d51c7175a58ce9b4bafefd945c
rosaann <147429240@qq.com>
2019-07-12 16:59:05 +0800

    d

--- dataset/dgl_dataset_factory.py
+++ dataset/dgl_dataset_factory.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 16:56:20 2019
+
+@author: zl
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -9,8 +17,8 @@ import torch.utils.data
 import torch.utils.data.sampler
 from torch.utils.data import DataLoader
 
-from .default import DefaultDataset, TestDataset
-#from .dgl_dataset import DGLDataset
+#from .default import DefaultDataset, TestDataset
+from .dgl_dataset import DGLDataset
 #from .small import SmallDataset
 #from .test import TestDataset
 
@@ -46,4 +54,13 @@ def get_test_dataloader( batch_size, **_):
                             pin_memory=False)
     return dataloader
 
+def get_gnu_dataloader(batch_size, split, **_):
+    dataset = DGLDataset(split)
 
+    dataloader = DataLoader(dataset,
+                            shuffle=False,
+                            batch_size=batch_size,
+                            drop_last=False,
+                            num_workers=6,
+                            pin_memory=False)
+    return dataloader
