788747ed826b757fe00825a70063b7b1f5818cf6
rosaann <147429240@qq.com>
2019-07-15 16:16:56 +0800

    d

---
+++ try_dgl.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 16:14:15 2019
+
+@author: zl
+"""
+
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import dgl.function as fn
+import torch
+import torch.nn as nn
+
+
+# Sends a message of node feature h.
+msg = fn.copy_src(src='h', out='m')
+
+def reduce(nodes):
+    """Take an average over all neighbor node features hu and use it to
+    overwrite the original node feature."""
+    accum = torch.mean(nodes.mailbox['m'], 1)
+    return {'h': accum}
+
+class NodeApplyModule(nn.Module):
+    """Update the node feature hv with ReLU(Whv+b)."""
+    def __init__(self, in_feats, out_feats, activation):
+        super(NodeApplyModule, self).__init__()
+        self.linear = nn.Linear(in_feats, out_feats)
+        self.activation = activation
+
+    def forward(self, node):
+        h = self.linear(node.data['h'])
+        h = self.activation(h)
+        return {'h' : h}
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, out_feats, activation):
+        super(GCN, self).__init__()
+        self.apply_mod = NodeApplyModule(in_feats, out_feats, activation)
+
+    def forward(self, g, feature):
+        # Initialize the node features with h.
+        g.ndata['h'] = feature
+        g.update_all(msg, reduce)
+        g.apply_nodes(func=self.apply_mod)
+        return g.ndata.pop('h')
+class Classifier(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_classes):
+        super(Classifier, self).__init__()
+
+        self.layers = nn.ModuleList([
+            GCN(in_dim, hidden_dim, F.relu),
+            GCN(hidden_dim, hidden_dim, F.relu)])
+        self.classify = nn.Linear(hidden_dim, n_classes)
+
+    def forward(self, g):
+        # For undirected graphs, in_degree is the same as
+        # out_degree.
+        h = g.in_degrees().view(-1, 1).float()
+        for conv in self.layers:
+            h = conv(g, h)
+        g.ndata['h'] = h
+        hg = dgl.mean_nodes(g, 'h')
+        return self.classify(hg)
+# Create training and test sets.
+trainset = MiniGCDataset(320, 10, 20)
+testset = MiniGCDataset(80, 10, 20)
+# Use PyTorch's DataLoader and the collate function
+# defined before.
+data_loader = DataLoader(trainset, batch_size=32, shuffle=True,
+                         collate_fn=collate)
+
+# Create model
+model = Classifier(1, 256, trainset.num_classes)
+loss_func = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+model.train()
+
+epoch_losses = []
+for epoch in range(80):
+    epoch_loss = 0
+    for iter, (bg, label) in enumerate(data_loader):
+        prediction = model(bg)
+        loss = loss_func(prediction, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.detach().item()
+    epoch_loss /= (iter + 1)
+    print('Epoch {}, loss {:.4f}'.format(epoch, epoch_loss))
+    epoch_losses.append(epoch_loss)
