db5e2b3143aae634da6ea3a9829f41c400b1080d
200445028 <200445028@stud.buct.edu.cn>
2019-07-27 17:13:55 +0800

    d

--- models/gnc.py
+++ models/gnc.py
@@ -199,7 +199,7 @@ class RGCNLayer(nn.Module):
         g.update_all(message_func, fn.sum(msg='msg', out='h2'), apply_func)
         
 class Regression_X1(nn.Module):
-    def __init__(self, in_dim = 4, h_dim = 48, out_dim = 1, num_rels = 1,
+    def __init__(self, in_dim = 4, h_dim = 64, out_dim = 1, num_rels = 1,
                  num_bases=-1, num_hidden_layers=2):
         super(Regression_X1, self).__init__()
         self.in_dim = in_dim
@@ -208,6 +208,7 @@ class Regression_X1(nn.Module):
         #self.num_rels = num_rels
         self.type_index_dic = find_type_index_dic()
         self.num_rels = len( self.type_index_dic.keys())
+        print('self.num_rels ', self.num_rels)
         self.num_hidden_layers = num_hidden_layers
         self.num_bases = num_bases
         # create rgcn layers
