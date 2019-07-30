047385352c19540a2f8af77cf179d01072b34ad0
200445028 <200445028@stud.buct.edu.cn>
2019-07-30 22:15:40 +0800

    d

--- models/gnc.py
+++ models/gnc.py
@@ -315,7 +315,7 @@ class Regression_X1(nn.Module):
             print('h2 ', h.shape)
             return {'h': h}
          g.apply_edges(edge_message_func_in)
-         return g.edata.pop('r')
+         return g.edata.pop('r')['wd']
         # g.update_all(message_func_in, reduce_func_in, apply_func_in)
     
     def build_h0_layer(self, in_feat, out_feat, num_rels, num_bases=-1, bias=None,
