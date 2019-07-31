814af1e9e581f73001022b5b0562b33a753e4bc8
rosaann <147429240@qq.com>
2019-07-31 17:32:57 +0800

    d

--- dataset/dgl_dataset.py
+++ dataset/dgl_dataset.py
@@ -115,7 +115,7 @@ def gen_train_data():
             scalar_coupling = row['scalar_coupling_constant']            
             train_data['edges'].append({'index0' : index_0, 'index1' : index_1, 'et' : t_index, 'sc' : float(scalar_coupling)})
          
-            if i > 10:
+            if i > 1000:
                 break
         train_data_list.append(train_data)
         
