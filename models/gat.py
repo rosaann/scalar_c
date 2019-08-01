f5066180cf1e63db8a6e45665994d936aa2deea0
rosaann <147429240@qq.com>
2019-08-01 14:30:01 +0800

    d

--- models/gat.py
+++ models/gat.py
@@ -139,7 +139,7 @@ class MultiHeadGATLayer(nn.Module):
             return torch.mean(torch.stack(head_outs))
         
 class GAT_X1(nn.Module):
-    def __init__(self, in_dim = 4, hidden_dim = 128, out_dim = 1, num_heads = 2):
+    def __init__(self, in_dim = 4, hidden_dim = 128, out_dim = 1, num_heads = 1):
         super(GAT_X1, self).__init__()
         self.type_index_dic = find_type_index_dic()
         self.num_rels = len( self.type_index_dic.keys())
@@ -154,8 +154,9 @@ class GAT_X1(nn.Module):
         g = self.layer1(g)
         print('forward 2')
         g = F.elu(g)
-        print('forward 3')
+        
         g = self.layer2(g)
+        print('forward 3')
         g.ndata.pop('h')
         result = g.edata.pop('r')
         return result
