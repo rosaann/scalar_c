4962bdd17ecb0a0d3d6f350ab0d7f809a40b74a1
rosaann <147429240@qq.com>
2019-08-07 10:02:08 +0800

    d

--- models/py_gcn.py
+++ models/py_gcn.py
@@ -110,7 +110,41 @@ class NodeModel(torch.nn.Module):
         out = self.node_mlp_2(out) 
         print('node_model out 5', out.shape, ' ', out)
         return out
+class MetaLayer_t(torch.nn.Module):
+    def __init__(self, edge_model=None, node_model=None):
+        super(MetaLayer, self).__init__()
+        self.edge_model = edge_model
+        self.node_model = node_model
+   #     self.global_model = global_model
 
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for item in [self.node_model, self.edge_model]:
+            if hasattr(item, 'reset_parameters'):
+                item.reset_parameters()
+
+    def forward(self, x, edge_index, edge_attr=None):
+        """"""
+        row, col = edge_index
+
+        if self.edge_model is not None:
+            edge_attr = self.edge_model(x[row], x[col], edge_attr)
+
+        if self.node_model is not None:
+            x = self.node_model(x, edge_index, edge_attr)
+
+        
+
+        return x, edge_attr
+
+    def __repr__(self):
+        return ('{}(\n'
+                '    edge_model={},\n'
+                '    node_model={},\n'
+                '    global_model={}\n'
+                ')').format(self.__class__.__name__, self.edge_model,
+self.node_model)
 def PY_MetaLayer ():
-    m = MetaLayer(EdgeModel(), NodeModel())
+    m = MetaLayer_t(EdgeModel(), NodeModel())
     return m
